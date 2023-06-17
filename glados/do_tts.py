a0a21b3a3c250363b747fb0b6fe6633c3c8d6e9f
Alexander Despotakis <acdespotakis@gmail.com>
2023-06-16 23:09:10 -0400

    Tweeks to import autotune scale

---
+++ glados/do_tts.py
@@ -0,0 +1,146 @@
+"""Make any piece of text sound like GlaDOS."""
+import typing
+from pathlib import Path
+
+import click
+import torch
+import torchaudio
+
+from glados.utils.auto_tune import Pitch, Scale, autotune
+from tortoise.api import MODELS_DIR, TextToSpeech
+from tortoise.utils.audio import load_voices
+
+_DEFAULT_VOICE = "glados"
+_ROOT_DIR = Path(__file__).parents[1]
+
+
+def _get_valid_pitches() -> list[str]:
+    """Return all valid pitches to tune to."""
+    valid: list[str] = []
+    for p in Pitch:
+        valid.extend((f"{p.name}:min", f"{p.name}:maj"))
+    return valid
+
+
+def _save(
+    gen: torch.Tensor | list[torch.Tensor],
+    dbg_state: typing.Any | None,
+    output_path: Path,
+) -> list[Path]:
+    """Save generated tensors."""
+    if not isinstance(gen, list):
+        gen = [gen]
+    output_files: list[Path] = []
+    for i, array in enumerate(gen):
+        p = output_path / f"{_DEFAULT_VOICE}_{i}.wav"
+        torchaudio.save(  # pylint: disable=no-member
+            str(p),
+            array.squeeze(0).cpu(),
+            24_000,
+        )
+        output_files.append(p)
+    if dbg_state:
+        ds_path = _ROOT_DIR / "debug_states"
+        ds_path.mkdir(exist_ok=True, parents=True)
+        torch.save(dbg_state, str(ds_path / f"do_tts_debug_{_DEFAULT_VOICE}.pth"))
+    return output_files
+
+
+@click.command()
+@click.option("-t", "--text", help="Text to speak.", type=str, required=True)
+@click.option(
+    "-p",
+    "--preset",
+    help="Inference quality preset.",
+    type=click.Choice(["ultra_fast", "fast", "standard", "high_quality"]),
+    default="fast",
+    show_default=True,
+)
+@click.option(
+    "-op",
+    "--output_path",
+    help="Where to store outputs.",
+    default=_ROOT_DIR / "results",
+    type=Path,
+    show_default=True,
+)
+@click.option(
+    "-md",
+    "--model-dir",
+    help="Where to find pretrained model checkpoints. Tortoise automatically \
+    downloads these to .models, so this should only be specified if you have \
+    custom checkpoints.",
+    default=MODELS_DIR,
+)
+@click.option(
+    "-c",
+    "--candidates",
+    help="How many output candidates to produce per-voice.",
+    type=int,
+    default=3,
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--seed",
+    type=int,
+    help="Random seed which can be used to reproduce results.",
+    default=None,
+)
+@click.option(
+    "-d",
+    "--produce-debug-state",
+    is_flag=True,
+    default=False,
+    help="Whether to produce debug_state.pth, which can aid in reproducing problems.",
+    show_default=True,
+)
+@click.option(
+    "-cvvp",
+    "--cvvp-amount",
+    type=float,
+    default=0.0,
+    help="How much the CVVP model should influence the output. Increasing \
+    this can in some cases reduce the likelihood of multiple speakers.",
+    show_default=True,
+)
+@click.option(
+    "-mk",
+    "--music-key",
+    type=click.Choice(_get_valid_pitches()),
+    help="Pitch:Key to scale to.",
+    default="A:min",
+    show_default=True,
+)
+def do_tts(  # pylint: disable=too-many-locals
+    text: str,
+    preset: str,
+    output_path: Path,
+    model_dir: str,
+    candidates: int,
+    seed: int | None,
+    produce_debug_state: bool,
+    cvvp_amount: float,
+    music_key: str,
+):
+    """GlaDOS-ify any text."""
+    output_path.mkdir(parents=True, exist_ok=True)
+    tts = TextToSpeech(models_dir=model_dir)
+    voice_samples, conditioning_latents = load_voices((_DEFAULT_VOICE,))
+    gen, dbg_state = tts.tts_with_preset(
+        text=text,
+        k=candidates,
+        voice_samples=voice_samples,
+        conditioning_latents=conditioning_latents,
+        preset=preset,
+        use_deterministic_seed=seed,
+        return_deterministic_state=True,
+        cvvp_amount=cvvp_amount,
+    )
+    output_files = _save(gen, dbg_state if produce_debug_state else None, output_path)
+    for of in output_files:
+        autotune(of, Scale.from_str(music_key))
+
+
+if __name__ == "__main__":
+    do_tts()  # pylint: disable=no-value-for-parameter
---
+++ glados/utils/auto_tune.py
@@ -0,0 +1,143 @@
+"""autotune"""
+import enum
+from pathlib import Path
+from typing import NamedTuple
+
+import librosa
+import librosa.display
+import numpy as np
+import psola
+import scipy.signal as sig
+import soundfile as sf
+
+_SEMITONES_IN_OCTAVE = 12
+
+
+class Pitch(enum.IntEnum):
+    """Key to scale to."""
+
+    A = enum.auto()
+    B = enum.auto()
+    C = enum.auto()
+    D = enum.auto()
+    E = enum.auto()
+    F = enum.auto()
+    G = enum.auto()
+
+
+class Key(enum.IntEnum):
+    """Minor / Major tone"""
+
+    MIN = enum.auto()
+    MAJ = enum.auto()
+
+
+class Scale(NamedTuple):
+    """Scale to autotune to"""
+
+    pitch: Pitch
+    key: Key
+
+    @classmethod
+    def from_str(cls, s: str) -> "Scale":
+        """Convert from librosa scale to enum"""
+        pitch, key = s.split(":")
+        return cls(getattr(Pitch, pitch.upper()), getattr(Key, key.upper()))
+
+    @property
+    def to_str(self) -> str:
+        """Conver to Librosa scale format"""
+        return f"{self.pitch.value}:{self.key.value}"
+
+
+def _autotune(audio: np.ndarray, sample_rate: float, scale: Scale | None = None):
+    """Autotune to pitch with PYIN algorithm + PSOLA algorithm."""
+    # Set some basis parameters.
+    frame_length = 2048
+    hop_length = frame_length // 4
+    fmin = librosa.note_to_hz("C2")
+    fmax = librosa.note_to_hz("C7")
+    wav_snippet, _, __ = librosa.pyin(
+        audio,
+        frame_length=frame_length,
+        hop_length=hop_length,
+        sr=sample_rate,
+        fmin=fmin,  # type: ignore
+        fmax=fmax,  # type: ignore
+    )
+    corrected_wav_snippet: np.ndarray
+    if scale:
+        corrected_wav_snippet = _tune_to_scale(wav_snippet, scale.to_str)
+    else:
+        corrected_wav_snippet = _tune_to_closest(wav_snippet)
+
+    # Pitch-shifting using the PSOLA algorithm.
+    return psola.vocode(
+        audio,
+        sample_rate=int(sample_rate),
+        target_pitch=corrected_wav_snippet,
+        fmin=fmin,  # type: ignore
+        fmax=fmax,  # type: ignore
+    )
+
+
+def _degrees_from(scale: str):
+    """Return the pitch classes (degrees) that correspond to the given scale"""
+    degrees = librosa.key_to_degrees(scale)
+    degrees = np.concatenate(
+        (
+            degrees,
+            [degrees[0] + _SEMITONES_IN_OCTAVE],
+        )
+    )
+    return degrees
+
+
+def _tune_to_closest(wav_snippet: np.ndarray):
+    """Round the given pitch values to the nearest MIDI note numbers"""
+    midi_note = np.around(librosa.hz_to_midi(wav_snippet))
+    nan_indices = np.isnan(wav_snippet)
+    midi_note[nan_indices] = np.nan
+    return librosa.midi_to_hz(midi_note)
+
+
+def _tune_to_scale(wav_snippet: np.ndarray, scale: str) -> np.ndarray:
+    """Map each pitch in the wav_snippet array to the closest
+    pitch belonging to the given scale."""
+    sanitized_pitch = np.zeros_like(wav_snippet)
+    for i in np.arange(wav_snippet.shape[0]):
+        sanitized_pitch[i] = _tune_to_scale_helper(wav_snippet[i], scale)
+    smoothed_sanitized_pitch = sig.medfilt(sanitized_pitch, kernel_size=11)
+    smoothed_sanitized_pitch[np.isnan(smoothed_sanitized_pitch)] = sanitized_pitch[
+        np.isnan(smoothed_sanitized_pitch)
+    ]
+    return smoothed_sanitized_pitch
+
+
+def _tune_to_scale_helper(wav_snippet: np.ndarray, scale: str):
+    """Return the pitch closest to wav_snippet that belongs to the given scale"""
+    if np.isnan(wav_snippet):
+        return np.nan
+    degrees = _degrees_from(scale)
+    midi_note = librosa.hz_to_midi(wav_snippet)
+    degree = midi_note % _SEMITONES_IN_OCTAVE
+    degree_id = np.argmin(np.abs(degrees - degree))
+    degree_difference = degree - degrees[degree_id]
+    midi_note -= degree_difference
+    return librosa.midi_to_hz(midi_note)
+
+
+def autotune(filepath: Path, scale: Scale | None = None) -> None:
+    """
+    Autotune some audio recording
+
+    :param filepath: Path to some file containing sound to autotune.
+    :param scale: If provided, autotune to this exact pitch.
+    """
+    y, sr = librosa.load(str(filepath), sr=None, mono=False)
+    if y.ndim > 1:
+        print("Converting stereo sound to mono.")
+        y = y[0, :]
+    pitch_corrected_y = _autotune(y, sr, scale)
+    filepath = filepath.parent / (filepath.stem + "_pitch_corrected" + filepath.suffix)
+    sf.write(str(filepath), pitch_corrected_y, sr)
